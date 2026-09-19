e2a5fd79fd7893080213603b8f9588dce5c8673b
Milos <miloslazic458@gmail.com>
2026-09-19 14:04:09 +0200

    niz celih, najveci broj, pozicija

---
+++ niz celih/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+int[] niz = new int[10];
+Random x = new Random();
+int max = 0;
+int position = 0;
+for (int i = 0; i < niz.Length; i++)
+{
+    niz[i] = x.Next(1, 100);
+    if(niz[i] > max)
+    {
+        max = niz[i];
+        position = i;
+    }
+    Console.WriteLine("Ispis Elemenata: " + niz[i]);
+}
+Console.WriteLine("Najveci broj u nizu je: " + max);
+Console.WriteLine("Pozicija najveceg broja u nizu je: " + position);
