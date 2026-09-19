02c44fe64bae3712b4245a8e4c2ff41bd1d574ab
Milos <miloslazic458@gmail.com>
2026-09-19 14:51:47 +0200

    Add files via upload

---
+++ while petlja/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Unesite broj n: ");
+int n = int.Parse(Console.ReadLine());
+
+List<int> niz = new List<int>();
+
+Console.Write("Unesite broj: ");
+int a = int.Parse(Console.ReadLine());
+
+while (a <= n)
+{
+    niz.Add(a);
+
+    Console.Write("Unesite broj: ");
+    a = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine($"Uneto je {niz.Count} brojeva: {string.Join(", ", niz)}");
