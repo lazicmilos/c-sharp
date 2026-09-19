4c3fa0897c2f8d26a93ac3111c892c96e4420485
Milos <miloslazic458@gmail.com>
2026-09-19 13:54:58 +0200

    Ispis parnih brojeva od 1 do n. Zbir parnih

---
+++ ispis parnih/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization.Formatters;
+Console.WriteLine("Enter number n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int zbir = 0;
+for (int i=0;i<=n;i++)
+{
+    if(i%2==0)
+       Console.WriteLine(i);
+    zbir += i;
+}
+Console.WriteLine("Zbir : " + zbir);
