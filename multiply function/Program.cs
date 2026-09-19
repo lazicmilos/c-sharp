0b482e09558d2b953b1238f7fb8bea2e174be007
Milos <miloslazic458@gmail.com>
2026-09-19 14:29:02 +0200

    Add files via upload

---
+++ multiply function/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+  {
+    public static int mnozenje(int a, int b)
+    {
+        return a * b;
+    }
+
+    static void Main()
+    {
+        int[] niz = new int[10];
+        Random x = new Random();
+        int n;
+        Console.WriteLine("Unesite broj: ");
+        n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < 10; i++)
+        {
+            niz[i] = x.Next(1, 100);
+            mnozenje(niz[i], n);
+            Console.WriteLine($"Broj {niz[i]} pomnozen sa {n} je: {mnozenje(niz[i], n)} ");
+        }
+    }
+}
+
